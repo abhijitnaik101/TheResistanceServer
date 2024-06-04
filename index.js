22210a0ebf31a6f532b7870e2dc69f009f9fd001
Abhijit Naik <abhijitnaik101@gmail.com>
2024-06-04 07:00:42 +0530

    changed origin

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://the-resistance.vercel.app/game",
+    origin: "https://the-resistance.vercel.app",
     methods: ["GET", "POST"]
   }
 })
